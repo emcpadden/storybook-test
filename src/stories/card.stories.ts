d41c8e9045251a202b61a96b7fcd1568641a80a7
Ed McPadden <edward.mcpadden@tangoe.com>
2019-09-04 18:36:48 -0400

    added storybook addons and added examples

--- src/stories/card.stories.ts
+++ src/stories/card.stories.ts
@@ -1,61 +1,70 @@
-import { storiesOf, moduleMetadata } from '@storybook/angular';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-import { withNotes } from '@storybook/addon-notes';
+import { storiesOf, moduleMetadata } from "@storybook/angular";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+import { withNotes } from "@storybook/addon-notes";
+import { withA11y } from "@storybook/addon-a11y";
+//You may need to import other things from knobs
+//like object, number, string etc
+import { withKnobs, text } from "@storybook/addon-knobs";
+import { withCssResources } from "@storybook/addon-cssresources";
+import { CardComponent } from "../app/card/card.component";
 
-import { CardComponent } from '../app/card/card.component';
+import { MatButtonModule } from "@angular/material";
 
-import {
-    MatButtonModule,
-} from '@angular/material';
-
-
-storiesOf('Card', module)
-.addDecorator(
+storiesOf("Card", module)
+  .addDecorator(withA11y) //Accessibility
+  .addDecorator(withKnobs) //Prop manipulation
+  .addDecorator(withCssResources) //CSS selection
+  .addParameters({
+    cssresources: [
+      {
+        id: `bluetheme`,
+        code: `<style>body { background-color: lightblue; }</style>`,
+        picked: false
+      }
+    ]
+  })
+  .addDecorator(
     moduleMetadata({
-        declarations: [
-            CardComponent
-        ],
-        imports: [
-            MatButtonModule,
-        ],
-    }),
-)
-  .add('empty', () => ({
+      declarations: [CardComponent],
+      imports: [MatButtonModule]
+    })
+  )
+  .add("empty", () => ({
     component: CardComponent,
     props: {}
   }))
-  .add('with title', () => ({
+  .add("with title", () => ({
     component: CardComponent,
     props: {
-      title: 'Hello card!'
+      title: "Hello card!"
     }
   }))
-  .add('with title and subtitle', () => ({
+  .add("with title and subtitle", () => ({
     component: CardComponent,
     props: {
-      title: 'Hello card!',
-      subtitle: 'Well hello there 👋'
+      title: "Hello card!",
+      subtitle: "Well hello there 👋"
     }
   }))
   .add(
-    'with notes',
-    withNotes('Just a few notes about this story...')(() => ({
+    "with notes",
+    withNotes("Just a few notes about this story...")(() => ({
       component: CardComponent,
       props: {}
     }))
   )
-  .add('with action', () => ({
+  .add("with action", () => ({
     component: CardComponent,
     props: {
-      title: 'A card...',
-      subtitle: 'Waiting to be clicked-on',
-      btnClicked: action('👊 Button was clicked')
+      title: text("Title", "A card...", "General"),
+      subtitle: "Waiting to be clicked-on",
+      btnClicked: action("👊 Button was clicked")
     }
   }));
 
 // let's nest a story into our main `Card` stories
-storiesOf('Card/nested', module).add('special card', () => ({
+storiesOf("Card/nested", module).add("special card", () => ({
   component: CardComponent,
   props: {
     content: "I'm a card in a nested story!"
